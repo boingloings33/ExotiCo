5930d8abe4545317442126615c4d78552810f3c0
NEPTR33 <baiolemi@gmail.com>
2023-09-26 07:46:16 -0400

    first commit

--- src/Components/Section1Web.jsx
+++ src/Components/Section1Web.jsx
@@ -1,121 +1,82 @@
-import {
-  Typography,
-  Stack,
-  Box,
-  Divider,
-  useTheme,
-  useMediaQuery,
-} from "@mui/material";
+import { Typography, Stack, Box } from "@mui/material";
 
+import Carousel from "react-material-ui-carousel";
+const fadeImages = [
+  {
+    id: 1,
+    src: "/images/allie-pic.webp",
+    title: "Title 1",
+    caption:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aspernatur fuga consequatur, asperiores deserunt ut eos corporis possimus quo vero aut ipsam rerum id! Ducimus harum debitis adipisci ipsum aut? asda oong, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt cumque optio laboriosam alias! Quo, quisquam voluptate. Minus quibusdam doloribus deserunt sapiente. Fuga magni architecto voluptates nobis dolores amet, sit earum.",
+  },
+  {
+    id: 2,
+    src: "/images/image-2.jpg",
+    title: "Title 2",
+    caption:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi obcaecati suscipit, dolor, necessitatibus libero iure ipsa quae earum perferendis incidunt tempora asperiores porro fuga magnam quibusdam dolore voluptate velit ea. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam illum sed, quos quidem saepe dolor accusantium at sint impedit assumenda veritatis optio iure, voluptate laboriosam voluptatibus! Reiciendis sapiente cupiditate voluptatem. recusandae Lorem ipsum dolor",
+  },
+  {
+    id: 3,
+    src: "/images/image-3.webp",
+    title: "Title 3",
+    caption:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, deserunt cupiditate, laboriosam sapiente dolore ad vel debitis deleniti delectus placeat eaque, ea illum corporis voluptate tenetur accusamus recusandae Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, laboriosam! Asperiores nihil, incidunt natus dicta similique doloribus! Optio sed alias, consectetur est suscipit omnis autem, dolorem placeat vel natus quos?  corporis voluptate tenetur ",
+  },
+];
 function Section1Web() {
-  const theme = useTheme();
-  const isTablet = useMediaQuery(theme.breakpoints.down("md"));
   return (
-    <Stack
-      alignItems="center"
-      justifyContent="center"
-      sx={{
-        backgroundColor: "primary.light",
-        py: 12,
-        width: "auto",
-        px: { sm: 8, lg: 30 },
+    <Carousel
+      swipe={false}
+      sx={{ overflow: "visible", py: 12 }}
+      autoPlay={false}
+      navButtonsAlwaysVisible={true}
+      fullHeightHover={false}
+      indicatorIconButtonProps={{
+        style: {
+          marginTop: "30px",
+        },
+      }}
+      navButtonsProps={{
+        style: {
+          backgroundColor: "rgba(18, 18, 18, 0.8)",
+          width: 64,
+          height: 64,
+        },
       }}
     >
-      <Box>
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Typography variant="h1">Title 1</Typography>
-          {isTablet && (
-            <img
-              src="./images/allie-pic.webp"
-              style={{ width: 240, height: 240, borderRadius: 16 }}
-            ></img>
-          )}
-        </Stack>
-        <Stack direction="row" spacing={4} alignItems="top">
-          <Typography
-            lineHeight={2}
-            variant="body2"
-            display="block"
-            sx={{ pt: 3 }}
-          >
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Consequatur dolorum praesentium nostrum ex doloremque, esse vel,
-            iste aliquid sit, explicabo Lorem ips? Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Architecto perspiciatis quisquam sequi
-            ex, ratione quas omnis maxi Deserunt omnis iusto porro eveniet esse
-            reiciendis dolore tempore orum praesentium nostrum el. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit.
-          </Typography>
-          <Typography
-            lineHeight={2}
-            variant="body2"
-            display="block"
-            sx={{ pt: 3 }}
-          >
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Consequatur dolorum praesentium nostrum ex doloremque, esse vel,
-            iste aliquid sit, explicabo Lorem ips? Deserunt omnis iusto porro
-            eveniet esse reiciendis dolore tempore orum praesentium nostrum el.
-            mnis maxi Deserunt omnis iusto porro eveniet esse reiciendis dolore
-            tempore orum praesentium nostrum el.
-          </Typography>
+      {fadeImages.map((img, i) => (
+        <Item key={i} img={img} />
+      ))}
+    </Carousel>
+  );
+}
 
-          {!isTablet && (
-            <img
-              style={{ objectFit: "cover", maxWidth: 374, borderRadius: 16 }}
-              src="./images/allie-pic.webp"
-              alt=""
-            />
-          )}
-        </Stack>
-        <Divider sx={{ mt: 10, mb: 10 }} />
-        <Stack direction="row" spacing={4} alignItems="top">
-          {isTablet && (
-            <img
-              src="./images/image-3.webp"
-              style={{
-                objectFit: "cover",
-                width: 300,
-                height: 490,
-                borderRadius: 16,
-              }}
-            />
-          )}
+function Item({ img }) {
+  return (
+    <Stack
+      justifyContent="center"
+      alignItems="center"
+      direction="row"
+      spacing={2}
+      sx={{ borderRadius: 6 }}
+    >
+      <Stack sx={{ maxWidth: 500 }} spacing={2}>
+        <Typography variant="h1">{img.title}</Typography>
+        <Typography sx={{ lineHeight: 2 }} variant="body1">
+          {img.caption}
+        </Typography>
+      </Stack>
 
-          {!isTablet && (
-            <img
-              src="./images/image-3.webp"
-              style={{
-                objectFit: "cover",
-                width: 705,
-                height: 497,
-                borderRadius: 16,
-              }}
-            />
-          )}
-          <Stack>
-            <Typography variant="h1">Title 2</Typography>
-            <Typography
-              lineHeight={2}
-              variant="body2"
-              display="block"
-              sx={{ pt: 3, maxWidth: 418 }}
-            >
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Consequatur dolorum praesentium nostrum ex doloremque, esse vel,
-              iste aliquid sit, explicabo Lorem ips? Deserunt omnis iusto porro
-              eveniet esse reiciendis dolore tempore orum praesentium nostrum
-              el. mnis maxi Deserunt omnis iusto porro eveniet esse reiciendis
-              dolore tempore orum praesentium nostrum el, doloribus aliquid sint
-              quam corrupti eum fuga minima!
-            </Typography>
-          </Stack>
-        </Stack>
-      </Box>
+      <img
+        style={{
+          objectFit: "cover",
+          width: 500,
+          height: 500,
+          borderRadius: 16,
+        }}
+        src={img.src}
+      ></img>
     </Stack>
   );
 }
